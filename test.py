ee1d22d2e356db9f92ee0dca196b2c4b4134056d
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2024-05-07 09:24:44 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,65 @@
+import cv2
+import pandas as pd
+import numpy as np
+from ultralytics import YOLO
+import cvzone
+import time
+
+model = YOLO('240_yolov8n_full_integer_quant_edgetpu.tflite')
+
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE:
+        colorsBGR = [x, y]
+        print(colorsBGR)
+
+cv2.namedWindow('RGB')
+cv2.setMouseCallback('RGB', RGB)
+
+cap = cv2.VideoCapture(0)
+
+my_file = open("coco.txt", "r")
+data = my_file.read()
+class_list = data.split("\n")
+
+frame_count = 0
+start_time = time.time()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame_count += 1
+    if frame_count % 3 != 0:
+        continue
+
+    results = model.predict(frame, imgsz=240)
+    a = results[0].boxes.data
+    px = pd.DataFrame(a).astype("float")
+
+    for index, row in px.iterrows():
+        x1 = int(row[0])
+        y1 = int(row[1])
+        x2 = int(row[2])
+        y2 = int(row[3])
+        d = int(row[5])
+        c = class_list[d]
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cvzone.putTextRect(frame, f'{c}', (x1, y1), 1, 1)
+
+    # Calculate FPS
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    fps = frame_count / elapsed_time
+
+    # Display FPS on frame
+    cvzone.putTextRect(frame, f'FPS: {round(fps, 2)}', (10, 30), 1,1)
+
+    cv2.imshow("RGB", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
